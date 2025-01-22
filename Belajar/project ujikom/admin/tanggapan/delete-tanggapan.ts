89927f59ebf7af16a1713e7f29f11a59810b6f8d
Khahaha10 <knurfattah@gmail.com>
2025-01-22 11:41:09 +0700

    test: fix missing delete button in delete-petugas page

--- Belajar/project ujikom/admin/tanggapan/delete-tanggapan.ts
+++ Belajar/project ujikom/admin/tanggapan/delete-tanggapan.ts
@@ -4,15 +4,21 @@ export class PlaywrightDeletePengaduan {
   readonly page: Page;
   readonly buttonHapusPengaduan: Locator;
   readonly checkModalDelete: Locator;
+  readonly buttonDelete: Locator;
+  readonly dashboardHeader: Locator
 
   constructor(page: Page) {
     this.page = page;
-    this.buttonHapusPengaduan = page.getByRole('button', { name: '' }).last();
+    this.buttonHapusPengaduan = page.getByRole('button', { name: '' }).first();
     this.checkModalDelete = page.getByRole('heading', { name: 'Hapus Pengaduan' });
+    this.buttonDelete = page.getByRole('button', { name: 'Hapus' });
+    this.dashboardHeader = page.getByRole('heading', { name: 'Pengaduan'}).locator('b');
   }
 
   async deletePengaduan(){
     await this.buttonHapusPengaduan.click();
     await expect(this.checkModalDelete).toBeVisible();
+    await this.buttonDelete.click();
+    await expect(this.dashboardHeader).toBeVisible();
   }
 }
